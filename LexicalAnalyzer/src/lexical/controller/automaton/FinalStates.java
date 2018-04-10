8d94bc685469eaa7847f84b735b5942848b772a8
Cássio Santos <sss.cassio@gmail.com>
2018-04-10 00:13:29 -0300

    Add States of comments, and string automaton

--- LexicalAnalyzer/src/lexical/controller/automaton/FinalStates.java
+++ LexicalAnalyzer/src/lexical/controller/automaton/FinalStates.java
@@ -14,8 +14,39 @@ public enum FinalStates implements FinalState {
         }
     },
     INDENTIFIER {
+        @Override
         public State next(char character) {
             return INDENTIFIER;
         }
-    }
+    },
+    ARI_SLASH {
+        @Override
+        public State next(char character) {
+            return ARI_SLASH;
+        }
+    },
+    LINE_COMMENT {
+        @Override
+        public State next(char character) {
+            return LINE_COMMENT;
+        }
+    },
+    BLOCK_COMMENT {
+        @Override
+        public State next(char character) {
+            return BLOCK_COMMENT;
+        }
+    },
+    STRING {
+        @Override
+        public State next(char character) {
+            return STRING;
+        }
+    },
+    INVALID_CHARACTER_ON_STRING {
+        @Override
+        public State next(char character) {
+            return INVALID_CHARACTER_ON_STRING;
+        }
+    },
 }
--- LexicalAnalyzer/src/lexical/controller/automaton/States.java
+++ LexicalAnalyzer/src/lexical/controller/automaton/States.java
@@ -8,6 +8,10 @@ public enum States implements State {
         public State next(char character) {
             if (LexemeType.isSpace(character)) {
                 return STATE_1;
+            } else if (character == '/') {
+                return STATE_3;
+            } else if (character == '\"') {
+                return STATE_10;
             } else if (LexemeType.isLetter(character)) {
                 return STATE_43;
             }
@@ -23,6 +27,77 @@ public enum States implements State {
             return FinalStates.WHITE_SPACE;
         }
     },
+    STATE_3 {
+        @Override
+        public State next(char character) {
+            if (character == '/') {
+                return STATE_4;
+            } else if (character == '*') {
+                return STATE_6;
+            }
+            return FinalStates.ARI_SLASH;
+        }
+    },
+    STATE_4 {
+        @Override
+        public State next(char character) {
+            if (character == '\n') {
+                return FinalStates.LINE_COMMENT;
+            }
+            return STATE_4;
+        }
+    },
+    STATE_6 {
+        // TODO: Verificar caso de chegar no fim do arquivo estando no estado 6 ou 7
+        @Override
+        public State next(char character) {
+            if (character == '*') {
+                return STATE_7;
+            }
+            return STATE_6;
+        }
+    },
+    STATE_7 {
+        // TODO: Verificar caso de chegar no fim do arquivo estando no estado 6 ou 7
+        @Override
+        public State next(char character) {
+            if (character == '/') {
+                return FinalStates.BLOCK_COMMENT;
+            } else if (character == '*') {
+                return STATE_7;
+            }
+            return STATE_6;
+        }
+    },
+    STATE_10 {
+        // TODO: Verificar caso de chegar no fim da linha estando no estado 10 ou 12
+        @Override
+        public State next(char character) {
+            if (character == '\\') {
+                return STATE_12;
+            } else if (character == '\"') {
+                return FinalStates.STRING;
+            } else if (LexemeType.isDigit(character) || LexemeType.isLetter(character)
+                    || LexemeType.isSymbol(character)) {
+                return STATE_10;
+            }
+            return FinalState.INVALID_CHARACTER_ON_STRING;
+        }
+    },
+    STATE_12 {
+        // TODO: Verificar caso de chegar no fim da linha estando no estado 10 ou 12
+        @Override
+        public State next(char character) {
+            if (character == '\\') {
+                return STATE_12;
+            } else if (character == '\"' || LexemeType.isDigit(character) || LexemeType.isLetter(character)
+                    || LexemeType.isSymbol(character)) {
+                return STATE_10;
+            }
+            return FinalState.INVALID_CHARACTER_ON_STRING;
+        }
+    },
+
     STATE_43 {
         @Override
         public State next(char character) {
